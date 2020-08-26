f02685e92ab42b3a108ac6df5223fbd57fc0ffa1
Xolog <stalk200213@gmail.com>
2020-08-26 23:01:47 +0300

    Create Хэш таблицы графов.py

---
+++ Хэш таблицы графов.py
@@ -0,0 +1,16 @@
+graph = {}
+graph["start"] = {} 
+graph["start"]["a"] = 6 
+graph["start"]["b"] = 2
+
+print(graph["start"].keys()) 
+
+print(graph["start"]["a"])
+print(graph["start"]["b"])
+
+graph["a"] = {} 
+graph["a"]["fin"] = 1 
+graph["b"] = {} 
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+graph["fin"] = {}
